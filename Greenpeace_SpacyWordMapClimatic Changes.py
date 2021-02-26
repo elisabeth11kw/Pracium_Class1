523f90a5ba38f26a401e8abc1aab61939469fedc
elisabeth wolfert <60304531+elisabeth11kw@users.noreply.github.com>
2021-02-26 16:54:21 -0700

    Updated Spacy file for text book
    This package can be easily changed to handle other text data.  I went to the guetenburg https://www.gutenberg.org/ site and downloaded a book on climate change.  Changed the name of the file and reran for attached.

---
+++ Greenpeace_SpacyWordMapClimatic Changes.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk import sent_tokenize, word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+import pandas as pd
+import numpy as np
+import datetime as dt 
+import re  
+import spacy
+nlp = spacy.load('en_core_web_lg')
+
+
+# In[2]:
+
+
+import datetime as dt 
+import pandas as pd 
+import os
+
+begin_date = dt.date(2020,7,1)
+end_date = dt.date(2020,7,13)
+
+
+limit = 100
+lang = 'english'
+
+# load file
+with open('C:/myfolderdata/letterC.txt', encoding="utf-8")  as df:
+    lines = df.readlines()
+  
+print (lines)
+      
+     
+    
+
+
+# In[3]:
+
+
+#This splits all the sentences up which makes it easier for us to work with
+
+all_sentences = []
+
+for word in lines:
+    all_sentences.append(word)
+
+all_sentences
+#df1 = df.to_string()
+
+#df_split = df1.split()
+
+#df_split
+lines = list()
+for line in all_sentences:    
+    words = line.split()
+    for w in words: 
+       lines.append(w)
+
+
+print(lines)
+
+
+# In[4]:
+
+
+#Removing Punctuation ^A-Za-z0-9
+
+lines = [re.sub(r'[Za]+', '', x) for x in lines]
+
+lines
+
+lines2 = []
+
+for word in lines:
+    if word != '':
+        lines2.append(word)
+
+
+# In[5]:
+
+
+#This is stemming the words to their root
+from nltk.stem.snowball import SnowballStemmer
+
+# The Snowball Stemmer requires that you pass a language parameter
+s_stemmer = SnowballStemmer(language='english')
+
+stem = []
+for word in lines2:
+    stem.append(s_stemmer.stem(word))
+    
+stem
+
+
+# In[6]:
+
+
+#Removing all Stop Words
+
+stem2 = []
+
+for word in stem:
+    if word not in nlp.Defaults.stop_words:
+        stem2.append(word)
+
+stem2
+
+
+# In[7]:
+
+
+df = pd.DataFrame(stem2)
+
+df = df[0].value_counts()
+#df
+#df['freq'] = df.groupby(0)[0].transform('count')
+#df['freq'] = df.groupby(0)[0].transform('count')
+#df.sort_values(by = ('freq'), ascending=False)
+
+
+# In[8]:
+
+
+#This will give frequencies of our words
+
+from nltk.probability import FreqDist
+
+freqdoctor = FreqDist()
+
+for words in df:
+    freqdoctor[words] += 1
+
+freqdoctor
+
+
+# In[9]:
+
+
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[10]:
+
+
+#This is a simple plot that shows the top 20 words being used
+#df.plot(20)
+
+df = df[:20,]
+plt.figure(figsize=(10,5))
+sns.barplot(df.values, df.index, alpha=0.8)
+plt.title('Top Words Overall')
+plt.ylabel('Word from Article', fontsize=12)
+plt.xlabel('Count of Words', fontsize=12)
+plt.show()
+
+
+# In[11]:
+
+
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+
+
+# In[12]:
+
+
+def show_ents(doc):
+    if doc.ents:
+        for ent in doc.ents:
+            print(ent.text + ' - ' + ent.label_ + ' - ' + str(spacy.explain(ent.label_)))
+
+
+# In[13]:
+
+
+str1 = " " 
+stem2 = str1.join(lines2)
+
+stem2 = nlp(stem2)
+
+label = [(X.text, X.label_) for X in stem2.ents]
+
+df6 = pd.DataFrame(label, columns = ['Word','Entity'])
+
+df7 = df6.where(df6['Entity'] == 'ORG')
+
+df7 = df7['Word'].value_counts()
+
+
+# In[14]:
+
+
+df = df7[:20,]
+plt.figure(figsize=(10,5))
+sns.barplot(df.values, df.index, alpha=0.8)
+plt.title('Top Organizations Mentioned')
+plt.ylabel('Word from Article', fontsize=12)
+plt.xlabel('Count of Words', fontsize=12)
+plt.show()
+
+
+# In[15]:
+
+
+str1 = " " 
+stem2 = str1.join(lines2)
+
+stem2 = nlp(stem2)
+
+label = [(X.text, X.label_) for X in stem2.ents]
+
+df10 = pd.DataFrame(label, columns = ['Word','Entity'])
+
+df10 = df10.where(df10['Entity'] == 'PERSON')
+
+df11 = df10['Word'].value_counts()
+
+
+# In[16]:
+
+
+df = df11[:20,]
+plt.figure(figsize=(10,5))
+sns.barplot(df.values, df.index, alpha=0.8)
+plt.title('Top People Mentioned')
+plt.ylabel('Word from Article', fontsize=12)
+plt.xlabel('Count of Words', fontsize=12)
+plt.show()
+
